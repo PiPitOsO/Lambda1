6dd3f4d02a822768776f835a16abf050e0e07452
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2020-07-18 18:35:54 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("тест");
-        System.out.println("тест");
+    public static void main(String[] args) throws ArithmeticException {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1); // будет 0
+        try {
+        int c = calc.devide.apply(a, b); // мы не любим делить на 0 = будет ошибка
+            calc.println.accept(c);
+        } catch (ArithmeticException m) {
+            System.out.println("И тут мы, так не красиво, упали с ошибкой."); // а я говорил
+        }
     }
 }
